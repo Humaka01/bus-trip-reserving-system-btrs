480e05f1c92593f0cf1348eeb9af5eda6de98517
Humaka01 <144069317+Humaka01@users.noreply.github.com>
2023-12-19 03:46:22 +0300

    Added initial project files

---
+++ BTRS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BTRS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
